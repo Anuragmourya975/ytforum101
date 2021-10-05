729f73330e8f1eed0f647a1b69206ad056d5e7e1
Aarti Jethnaiya <jethaniya20aarti@gmail.com>
2021-10-05 17:02:09 +0530

    add drawer

--- src/Components/Navbar101.jsx
+++ src/Components/Navbar101.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 // import Button from "react-bootstrap/Button";
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 import "./Navbar.css";
 import Form101 from "./Form101";
+import SidebarData from "./SideBarData"
 import {
   Container,
   NavDropdown,
@@ -13,9 +16,14 @@ import {
 
 function Navbar101() {
   const [modalShow, setModalShow] = React.useState(false);
+  const [sidebarShow, setSidebarShow] = React.useState(false);
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+    <>
+    <Navbar collapseOnSelect expand="lg" bg= "dark" variant="dark" fixed="top">
       <Container>
+        
+       
+        
         <Navbar.Brand href="#home">
           {" "}
           <img
@@ -58,19 +66,53 @@ function Navbar101() {
           onClick={() => setModalShow(true)}
           style={{ cursor: "pointer" }}
         >
-          {" "}
           <img
             alt=""
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/640px-YouTube_full-color_icon_%282017%29.svg.png"
             width="45"
             height="30"
             className="d-inline-block align-top"
-          />{" "}
+          />
         </Navbar.Brand>
         <Form101 show={modalShow} onHide={() => setModalShow(false)} />
+
+        <Navbar.Brand 
+          href = "#home"
+          style={{ cursor: "pointer" }}
+        >
+          <FaIcons.FaBars className = "menu-bar" onClick={() => setSidebarShow(!sidebarShow)}/>
+          <nav className = {sidebarShow ? 'drawer-menu active' : 'drawer-menu'}>
+            <ul className = "drawer-menu-items" onClick={() => setSidebarShow(!sidebarShow)}>
+               <li className = "drawer-toggle">
+                   <AiIcons.AiOutlineClose />
+               </li>
+               {SidebarData.map((item,index) =>{
+                   return(
+                       
+                            <li key = {index} className = {item.cName}>
+                                <img
+                                    src={item.icons}
+                                    width="30"
+                                    height="30"
+                                    className = "drawer-item-div"
+                                />
+                                <span className = "drawer-item-div">{item.title}</span>
+                            </li>
+                        
+                   );
+               })}
+            </ul>     
+          </nav>
+        </Navbar.Brand>
+       
       </Container>
     </Navbar>
+    <br/>
+    <br/>
+    
+    </>
   );
 }
 
 export default Navbar101;
+// bg="dark"
---
+++ src/Components/SideBarData.jsx
@@ -0,0 +1,54 @@
+import '../images/web.png';
+const SidebarData = [
+    {
+        title : 'Web Developement',
+        path : "/",
+        icons : "images/web.png",
+        cName : "drawer-text",
+        
+    },
+    {
+        title : 'App Developement',
+        path : "/",
+        cName : "drawer-text",
+        icons : "app",
+    },
+    {
+        title : 'Data Science',
+        path : "/",
+        cName : "drawer-text",
+        icons : "data",
+    },
+    {
+        title : 'Programming Language',
+        path : "/",
+        cName : "drawer-text",
+        icons : "language",
+    },
+    {
+        title : 'Academic',
+        path : "/",
+        cName : "drawer-text",
+        icons : "academic",
+    },
+    {
+        title : 'Apptitude ',
+        path : "/",
+        cName : "drawer-text",
+        icons : "apti",
+    },
+    {
+        title : 'DSA and CP',
+        path : "/",
+        icons : "cp",
+        cName : "drawer-text",
+    },
+    {
+        title : 'Interview Preparation',
+        path : "/",
+        icons : "interview",
+        cName : "drawer-text",
+    },
+];
+
+export default SidebarData;
